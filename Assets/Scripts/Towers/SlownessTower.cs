703dbb5ebbcea3a71f17f61558e51040ad33a723
Yak.Space <61755971+BobCat-Space@users.noreply.github.com>
2022-05-05 11:54:52 +0100

    qqqqq

--- Assets/Scripts/Towers/SlownessTower.cs
+++ Assets/Scripts/Towers/SlownessTower.cs
@@ -7,14 +7,12 @@ public class SlownessTower : DebuffTower
     public float slowModifier;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Enemy")
-        {
-            other.GetComponent<SplineFinding>().speedModifier = slowModifier;
-        }
+        Debug.Log(other);
+        other.GetComponent<SplineFinding>().speedModifier = other.GetComponent<SplineFinding>().speedModifier * slowModifier;
     }
     private void OnTriggerExit(Collider other)
     {
-        other.GetComponent<SplineFinding>().speedModifier = 0.2f;
+        other.GetComponent<SplineFinding>().speedModifier = 0.1f;
         GetComponent<AudioSource>().PlayOneShot(shootSound);
     }
 }
